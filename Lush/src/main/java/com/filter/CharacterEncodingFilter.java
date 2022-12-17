73d01b6011d1a036881e0c1afb46ed09e0b73112
jujinyoung <jujinyoung1838@naver.com>
2022-12-18 00:25:12 +0900

    fix: CharacterEncodingFilter 에러 CharacterEncodingFilter init부분 jsp가 렌더링 되지않는 에러 수정 -> init주석처리

--- Lush/src/main/java/com/filter/CharacterEncodingFilter.java
+++ Lush/src/main/java/com/filter/CharacterEncodingFilter.java
@@ -10,15 +10,15 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         // web.xml -> filter등록  encoding=UTF-8
-        this.encoding =  filterConfig.getInitParameter("encoding");
-        if( this.encoding == null )  encoding="UTF-8";
+//        this.encoding =  filterConfig.getInitParameter("encoding");
+//        if( this.encoding == null )  encoding="UTF-8";
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        //
-        System.out.println(">CharacterEncodingFilter.doFilter(); 호출됨");
+
+//        System.out.println(">CharacterEncodingFilter.doFilter(); 호출됨");
         request.setCharacterEncoding("UTF-8");
 
         chain.doFilter(request, response); // 다음 체인이 있다면 이동
