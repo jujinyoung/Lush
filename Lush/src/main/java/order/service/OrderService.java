b9aabfc2136eef7fde7a2a66990c6964eba27952
Jihun-Kim <anam4561@naver.com>
2022-12-19 14:42:32 +0900

    feat: order test

---
+++ Lush/src/main/java/order/service/OrderService.java
@@ -0,0 +1,37 @@
+package order.service;
+
+import com.util.ConnectionProvider;
+import event.dao.EventDao;
+import event.dao.EventDaoImpl;
+import event.domain.Event;
+import event.exception.EventListEmptyException;
+
+import javax.naming.NamingException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class OrderListService {
+    private static EventListService instance = new EventListService();
+    private EventListService(){}
+    public static EventListService getInstance(){return instance;}
+
+    public List<Event> selectEventList(){
+        Connection conn = null;
+        EventDao dao = EventDaoImpl.getInstance();
+        List<Event> events = null;
+
+        try {
+            conn = ConnectionProvider.getConnection();
+            events = dao.selectEventList(conn);
+            if (events == null){
+                throw new EventListEmptyException("이벤트 목록이 비어있습니다.");
+            }
+        } catch (NamingException | SQLException e) {
+            e.printStackTrace();
+        } catch (EventListEmptyException e) {
+            e.printStackTrace();
+        }
+        return events;
+    }
+}
